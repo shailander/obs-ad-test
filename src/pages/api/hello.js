3cc58c59101b762587cbd60e146b6af476781ebc
shailander <shailander.singh.111@gmail.com>
2023-06-08 17:34:26 +0530

    basic setup done

---
+++ src/pages/api/hello.js
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+require("dotenv").config();
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    // Access the data sent in the request body
+    const { isVisible } = req.body;
+    console.log(isVisible, "isVisible");
+
+    const insertQuery = "INSERT INTO test (isVisible) VALUES (?)";
+    const values = [isVisible];
+
+    // Perform any necessary processing or validations here
+    connection.query(insertQuery, values, (error, results) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        // Handle the error response
+      } else {
+        console.log("Insert successful");
+        // Handle the success response
+      }
+    });
+    // connection.end();
+
+    // Return a JSON response with the data
+    res.status(200).json({ message: "Data received successfully", isVisible });
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import LogoImg from "../../public/logo.png";
+
+const TIME_TILL_SHOW = 15000; //show=true
+const TIME_TILL_HIDE = 45000; //show=false
+
+export default function Home() {
+  const isVisible = useRef(true);
+  const [showLogo, setShowLogo] = useState(false);
+  const timer = useRef(0);
+
+  useEffect(() => {
+    const isObsPresent = !!window?.obsstudio?.pluginVersion;
+
+    const intervalId = isObsPresent
+      ? setInterval(() => {
+          sendData();
+        }, 5000)
+      : null;
+
+    isVisible.current = isObsPresent;
+
+    // const ele = document.getElementById("damm-element");
+    // window.addEventListener("obsSourceVisibleChanged", function (event) {
+    //   ele.innerText = `Visible : ${event.detail.visible} ** Active : ${data.current.active}`;
+    //   data.current = { ...data.current, visible: event.detail.visible };
+    // });
+    window.addEventListener("obsSourceActiveChanged", function (event) {
+      // ele.innerText = `Visible : ${data.current.visible} ** Active : ${event.detail.active}`;
+      isVisible.current = !!event.detail.active;
+    });
+
+    // window.obsstudio.onVisibilityChange
+
+    // setTimeout(() => {
+    //   data.current = Math.random(1, 100);
+    // }, 1000);
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    let timerId;
+    if (showLogo) {
+      timerId = setTimeout(() => {
+        setShowLogo(false);
+      }, TIME_TILL_SHOW);
+    } else {
+      timerId = setTimeout(() => {
+        setShowLogo(true);
+      }, TIME_TILL_HIDE);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [showLogo]);
+
+  const sendData = async () => {
+    await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isVisible: isVisible.current,
+      }),
+    });
+  };
+  return (
+    <div className="h-screen w-screen bg-transparent text-2xl flex justify-start items-end p-4">
+      <img
+        src={LogoImg.src}
+        alt="logo"
+        height={200}
+        width={200}
+        className={`${
+          showLogo ? "opacity-100" : "opacity-0"
+        } transition-opacity`}
+      />
+    </div>
+  );
+}
